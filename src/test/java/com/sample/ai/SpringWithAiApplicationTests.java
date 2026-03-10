a2e59c4e5307ec396732199710252dfbfdeb6c2f
sandeep-yadav <sandeep.yadav@tothenew.com>
2026-03-10 12:13:08 +0530

    spring with AI sample code

---
+++ src/test/java/com/sample/ai/SpringWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
