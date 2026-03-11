38c99eea9da927f3bc0bec61b1835b71eea0f6cc
sandeep-yadav <sandeep.yadav@tothenew.com>
2026-03-11 11:53:51 +0530

    Integrating llama3.2 changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Adding README file
+Install llama3.2 below command can be used to install llama3.2
+
+curl -fsSL https://ollama.com/install.sh | sh
+
+Run the following command to install llama3.2:
+ollama run llama3.2
+
--- src/main/java/com/sample/ai/controller/ChatController.java
+++ src/main/java/com/sample/ai/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.sample.ai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ChatController {
 
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
     @GetMapping("/chat")
     public String chat(@RequestParam("message") String message) {
-       
-        return "You said: " + message;
+        return chatClient.prompt(message).call().content();
+
     }
 }
